c57f4a7e0efe45fa810687bde67f5e860b49f944
LiamRoyle-Grimes <141082592+LiamRoyle-Grimes@users.noreply.github.com>
2024-05-05 17:40:40 -0600

    commit

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1714951440|25684088";
+const CACHE_VERSION = "1714952424|120562981";
 const CACHE_PREFIX = "learning philoso-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
